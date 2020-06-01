832c4281e9dd859e665a63596e1743cf3f05ca40
Munioro <munioro27@gmail.com>
2020-06-01 00:31:55 -0500

    added manifest

---
+++ public/service-worker.js
@@ -0,0 +1,13 @@
+const FILES_TO_CACHE = [
+    '/',
+    '/index.html',
+    '/styles.css',
+    '/index.js',
+    '/manifest.webmanifest',
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png',
+    'https://cdn.jsdelivr.net/npm/chart.js@2.8.0'
+  ];
+
+  const CACHE_NAME = 'static-cache-v1';
+  const DATA_CACHE_NAME = 'data-cache-v1';
